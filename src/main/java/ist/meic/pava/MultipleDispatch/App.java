a589d9126f52d29ab1a3cd4ab32361c249e99107
Gabriel Almeida <galmeida9@gmail.com>
2021-03-30 16:02:34 +0100

    Implement main functionality
    Co-authored-by: Daniel Gonçalves <daniel.gdf99@gmail.com>

--- src/main/java/ist/meic/pava/MultipleDispatch/App.java
+++ src/main/java/ist/meic/pava/MultipleDispatch/App.java
@@ -6,8 +6,65 @@ package ist.meic.pava.MultipleDispatch;
  */
 public class App 
 {
+
+    public static class Shape {}
+    public static class Line extends Shape {}
+    public static class Circle extends Shape {}
+    public static class Brush {}
+    public static class Pencil extends Brush {}
+    public static class Crayon extends Brush {}
+    public static class Device {
+        public String draw(Shape s, Brush b) {
+            return "draw what where and with what?";
+        }
+        public String draw(Line l, Brush b) {
+            return "draw a line where and with what?";
+        }
+        public String draw(Circle c, Brush b) {
+            return "draw a circle where and with what?";
+        }
+    }
+    public static class Screen extends Device {
+        public String draw(Line l, Brush b) {
+            return "draw a line where and with what?";
+        }
+        public String draw(Line l, Pencil p) {
+            return "drawing a line on screen with pencil!";
+        }
+        public String draw(Line l, Crayon c) {
+            return "drawing a line on screen with crayon!";
+        }
+        public String draw(Circle c, Brush b) {
+            return "drawing a circle on screen with what?";
+        }
+        public String draw(Circle c, Pencil p) {
+            return "drawing a circle on screen with pencil!";
+        }
+    }
+    public static class Printer extends Device {
+        public String draw(Line l, Brush b) {
+            return "drawing a line on printer with what?";
+        }
+        public String draw(Circle c, Pencil p) {
+            return "drawing a circle on printer with pencil!";
+        }
+        public String draw(Circle c, Crayon r) {
+            return "drawing a circle on printer with crayon!";
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Device[] devices = new Device[] { new Screen(), new Printer() };
+        Shape[] shapes = new Shape[] { new Line(),new Circle() };
+        Brush[] brushes = new Brush[] {new Pencil(),new Crayon() };
+        for (Device device: devices) {
+            for(Shape shape: shapes) {
+                for(Brush brush: brushes) {
+                    System.out.println(UsingMultipleDispatch.invoke(device,"draw", shape, brush));
+                }
+            }
+        }
+        // System.out.println( "Hello World!" );
     }
 }
--- src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
+++ src/main/java/ist/meic/pava/MultipleDispatch/UsingMultipleDispatch.java
@@ -1,7 +1,80 @@
 package ist.meic.pava.MultipleDispatch;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
 public class UsingMultipleDispatch {
     public static Object invoke(Object receiver, String name, Object... args) {
-        throw new UnsupportedOperationException();
+        try {
+            Method method = bestMethod(receiver.getClass(), name,
+                    Arrays.stream(args).map(Object::getClass).toArray(Class[]::new));
+            return method.invoke(receiver, args);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e); // check exception
+        }
+    }
+
+    static class Node {
+        Class[] args;
+        int[] level;
+
+        Node(Class[] args, int[] level) {
+            this.args = args;
+            this.level = level;
+        }
+    }
+
+    private static List<Node> generateNode(Node node) {
+        List<Node> res = new LinkedList<>();
+        int lowestLevel = node.level[0];
+        for (int i = 0; i < node.level.length; i++) {
+            int tmp = node.level[i];
+            if (tmp < lowestLevel) lowestLevel = tmp;
+        }
+        for (int j = 0; j < node.args.length; j++) {
+            if (node.level[j] == lowestLevel && node.args[j].getSuperclass() != Object.class) {
+                Node newNode = new Node(node.args.clone(), node.level.clone());
+
+                newNode.args[j] = newNode.args[j].getSuperclass();
+                newNode.level[j]++;
+                res.add(newNode);
+            }
+        }
+        return res;
+    }
+
+    private static Method bestMethod(Class receiverType, String name, Class... argType) throws NoSuchMethodException {
+        try {
+            return receiverType.getMethod(name, argType);
+        } catch (NoSuchMethodException e) {
+            // Search in breadth -> left to right arguments
+            Node initialNode = new Node(argType, new int[argType.length]);
+
+            List<Node> visited = new LinkedList<>();
+            List<Node> queue = new LinkedList<>(Collections.singleton(initialNode));
+
+            Node currNode;
+            visited.add(initialNode);
+
+            while (!queue.isEmpty()) {
+                currNode = queue.remove(0);
+
+                List<Node> adjs = generateNode(currNode);
+
+                for (Node next : adjs) {
+                    if (visited.stream().noneMatch(node -> Arrays.equals(node.level, next.level))) {
+                        try {
+                            return receiverType.getMethod(name, next.args);
+                        } catch (NoSuchMethodException exception) {
+                            visited.add(next);
+                            queue.add(next);
+                        }
+                    }
+                }
+            }
+
+            throw e;
+        }
     }
 }
